7fbe8d1c58906915aa2e8bf36cfaf51a6c2c37f8
sshine <simon@simonshine.dk>
2024-11-19 15:32:13 +0100

    Slette kommuner

--- src/main/java/com/example/demo/controller/KommuneController.java
+++ src/main/java/com/example/demo/controller/KommuneController.java
@@ -5,11 +5,15 @@ import com.example.demo.model.Region;
 import com.example.demo.repository.KommuneRepository;
 import com.example.demo.repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class KommuneController {
@@ -31,5 +35,14 @@ public class KommuneController {
         return regionskode == null
             ? kommuneRepository.findAll()
             : kommuneRepository.findByRegion_Kode(regionskode);
+    }
+
+    @PostMapping("/kommuner/remove")
+    public void removeKommune(@RequestParam int id) {
+        Optional<Kommune> kommune = kommuneRepository.findById(id);
+        if (kommune.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Kommune ikke fundet");
+        }
+        kommuneRepository.delete(kommune.get());
     }
 }
